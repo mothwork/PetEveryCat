255cb598dbb6ce33e2f34454a303f4a21fd25c07
Brett Hageft <brett.hageft@gmail.com>
2021-11-15 15:16:40 -0700

    sign up route started

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { csrfProtection, asyncHandler } = require('../utils')
+const bcrypt = require('bcryptjs')
+const { check, validationResult } = require('express-validators')
+const db = require('../db/models')
+const { User } = db
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/sign-up', csrfProtection, asyncHandler(async (req, res) => {
+  const user = await User.build() // Does this need an await?
+  res.render('sign-up', {Title: 'Sign Up', user, csrfToken: req.csrfToken()})
+}))
+
 
 module.exports = router;
