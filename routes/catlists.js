d491ef92409a6b134272b462b7f7d5bc322c7928
David Nash <hisownspace@gmail.com>
2021-11-16 13:58:20 -0500

    resolving local environment issue

--- routes/catlists.js
+++ routes/catlists.js
@@ -1,13 +1,13 @@
-// const express = require("express");
-// const router = express.Router();
-// const { csrfProtection, asyncHandler } = require('../utils')
-// const db = require('../db/models')
-// const { User, CatList } = db
+const express = require("express");
+const router = express.Router();
+const { csrfProtection, asyncHandler } = require('../utils')
+const db = require('../db/models')
+const { User, CatList } = db
 
-// router.get('/', asyncHandler(async(req, res) => {
-//     const catLists = await CatList.findAll({ where: { userId: req.session.auth.userId } })
+router.get('/', asyncHandler(async(req, res) => {
+    const catLists = await CatList.findAll({ where: { userId: req.session.auth.userId } })
 
-//     res.render('cat-lists', { title: "My Cat Lists", catLists });
-// }))
+    res.render('cat-lists', { title: "My Cat Lists", catLists });
+}))
 
-// module.exports = router
+module.exports = router
