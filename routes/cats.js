9dfcb497f21f2b55a0cd81fbca6a4237d55e98e2
Gabriel Aspuria <gabrielmaspuria@gmail.com>
2021-11-16 13:36:12 -0800

    edit cat info done

--- routes/cats.js
+++ routes/cats.js
@@ -4,6 +4,7 @@ const db = require('../db/models')
 const { Cat, User } = db
 const { restoreUser } = require("../auth")
 const { check, validationResult } = require('express-validator')
+const e = require('express')
 
 const router = express.Router()
 
@@ -35,6 +36,29 @@ router.get('/new', csrfProtection, restoreUser, asyncHandler(async(req, res) => 
 
 const catValidators = [
     // TODO: when database constraints are determined
+    check('name')
+        .exists({checkFalsy: true})
+        .withMessage('Please provide a name')
+        .isLength({max: 50})
+        .withMessage('Name must be less than 50 characters'),
+    check('breed')
+        .exists({checkFalsy: true})
+        .withMessage('Please provide a breed')
+        .isLength({max: 50})
+        .withMessage('Breed must be less than 50 characters'),
+    check('size')
+        .exists({checkFalsy: true})
+        .withMessage('Please provide a size')
+        .isLength({max: 50})
+        .withMessage('Size must be less than 50 characters'),
+    check('friendly')
+        .exists({checkFalsy: true})
+        .withMessage('Please select an option for friendly'),
+    check('coat')
+        .exists({checkFalsy: true})
+        .withMessage('Please provide the color of the cat\'s coat')
+        .isLength({max: 50})
+        .withMessage('Color of coat must be less than 50 characters'),
 ]
 
 router.post('/new', csrfProtection, restoreUser, catValidators, asyncHandler(async(req, res) =>{
@@ -42,7 +66,7 @@ router.post('/new', csrfProtection, restoreUser, catValidators, asyncHandler(asy
     const newCat = await Cat.build({
         name, breed, size, friendly, coat, userId, imgUrl
     })
-    const errors = [];
+    // const errors = [];
     const validatorErrors = validationResult(req);
     if(validatorErrors.isEmpty()) {
         await newCat.save();
@@ -50,10 +74,33 @@ router.post('/new', csrfProtection, restoreUser, catValidators, asyncHandler(asy
         res.redirect(`/cats/${cat.id}`);
     } else {
         const errors = validatorErrors.array().map(e => {
-            e.msg
+           return e.msg
         })
         res.render('new-cat', {Title: 'New Cat', newCat, errors, csrfToken: req.csrfToken()})
 
+    }
+}))
+
+router.get('/edit/:id(\\d+)', csrfProtection, restoreUser, catValidators, asyncHandler(async(req, res) => {
+    const catId = req.params.id;
+    const cat = await Cat.findByPk(catId);
+
+    res.render('cats-edit', {Title: 'Edit Cat', cat, csrfToken: req.csrfToken()})
+}))
+
+router.post('/edit/:id(\\d+)', csrfProtection, restoreUser, catValidators, asyncHandler(async(req, res) => {
+    const catId = req.params.id;
+    const catToUpdate = await Cat.findByPk(catId);
+    const { name, breed, size, friendly, coat, imgUrl } = req.body;
+
+    const cat = { name, breed, size, friendly, coat, imgUrl }
+    const validatorErrors = validationResult(req);
+    if(validatorErrors.isEmpty()) {
+        await catToUpdate.update(cat);
+        res.redirect(`/cats/${catId}`);
+    } else {
+        const errors = validatorErrors.array().map(e => e.msg);
+        res.render('cats-edit', {Title: 'Edit Cat', cat, errors, csrfToken: req.csrfToken()});
     }
 }))
 
