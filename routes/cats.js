e9ea0b4b335278729f2c4f2c985863a4918cb963
Gabriel Aspuria <gabrielmaspuria@gmail.com>
2021-11-16 10:02:05 -0800

    router get and post and pug file for a  new cat made

--- routes/cats.js
+++ routes/cats.js
@@ -3,6 +3,7 @@ const { csrfProtection, asyncHandler } = require('../utils')
 const db = require('../db/models')
 const { Cat } = db
 const { restoreUser } = require("../auth")
+const { check, validationResult } = require('express-validator')
 
 const router = express.Router()
 
@@ -18,3 +19,32 @@ router.get('/:id(\\d+)', restoreUser, asyncHandler(async (req, res) => {
     res.render("cat-info", {Title: `${cat.name}`, cat}) //Does this work?
 }))
 
+router.get('/new', csrfProtection, restoreUser, asyncHandler(async(req, res) => {
+    const { userId } = req.session.auth
+    const cat = cat.build();
+    res.render('new-cat', {Title: 'New cat', csrfToken: req.csrfToken(), cat, userId});
+}));
+
+const catValidators = [
+    // TODO: when database constraints are determined
+]
+
+router.post('/new', csrfProtection, restoreUser, catValidators, asyncHandler(async(req, res) =>{
+    const { name, breed, size, friendly, coat, userId, imgUrl } = req.body;
+    const newCat = await Cat.build({
+        name, breed, size, friendly, coat, userId, imgUrl
+    })
+    const errors = [];
+    const validatorErrors = validationResult(req);
+    if(validatorErrors.isEmpty()) {
+        await newCat.save();
+        const cat = await Cat.findOne({ where: {name} });
+        res.redirect(`/cats/${cat.id}`);
+    } else {
+        const errors = validatorErrors.array().map(e => {
+            e.msg
+        })
+        res.render('new-cat', {Title: 'New Cat', newCat, errors, csrfToken: req.csrfToken()})
+
+    }
+}))
