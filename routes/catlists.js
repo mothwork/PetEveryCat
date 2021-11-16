e8b88daed038e41e442c53d587482e6eb43db058
Joe Yang (Joseph) <josephjyang@gmail.com>
2021-11-16 16:34:54 -0500

    Cat List Pug creation

--- routes/catlists.js
+++ routes/catlists.js
@@ -2,12 +2,34 @@ const express = require("express");
 const router = express.Router();
 const { csrfProtection, asyncHandler } = require('../utils')
 const db = require('../db/models')
-const { User, CatList } = db
+const { User, CatList, Cat, Review } = db
 
 router.get('/', asyncHandler(async(req, res) => {
-    const catLists = await CatList.findAll({ where: { userId: req.session.auth.userId } })
-
+    const catLists = await CatList.findAll({ where: { userId: req.session.auth.userId }, include: Cat })
     res.render('cat-lists', { title: "My Cat Lists", catLists });
+}))
+
+const catListNotFound = catListId => {
+    const error = new Error(`Cat List with ID ${catListId} could not be found`);
+    error.title = "Cat List not found.";
+    error.status = 404;
+    return error;
+};
+
+router.get('/:id(\\d+)', asyncHandler(async(req, res, next) => {
+    const catListId = req.params.id;
+    const catList = await CatList.findByPk(catListId, { include: {
+        model: Cat,
+        include: Review
+    } } );
+    console.log(catList.Cats);
+
+    if (!catList || catList.userId !== req.session.auth.userId) {
+        return next(catListNotFound(req.params.id));
+    }
+    else {
+        res.render('cat-list', { title: catList.name, catList })
+    }
 }))
 
 module.exports = router
