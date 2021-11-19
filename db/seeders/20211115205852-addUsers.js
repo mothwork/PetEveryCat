c61104dd818044baf5ee99f2f6aa325ab29b49bb
David Nash <hisownspace@gmail.com>
2021-11-19 12:06:45 -0500

    changed login to avoid race condition

--- db/seeders/20211115205852-addUsers.js
+++ db/seeders/20211115205852-addUsers.js
@@ -7,7 +7,7 @@ module.exports = {
    return await queryInterface.bulkInsert('Users', [
      { username: 'Demouser',hashedPassword: bcrypt.hashSync('P@ssw0rd', 10),firstName: "Demo",lastName: "User",bio: "This is a demo user so you may test out all functionality.",createdAt: new Date(),updatedAt: new Date()},
      { username: 'johndoe',hashedPassword: bcrypt.hashSync('P@ssw0rd', 10),firstName: "John",lastName: "Doe",bio: "I am a dummy account.",createdAt: new Date(),updatedAt: new Date()},
-     { username: 'catlover2000',hashedPassword: bcrypt.hashSync('P@ssw0rd', 10),firstName: "Blake",lastName: "Kindly",bio: "I'm real but you'll never find me.",createdAt: new Date(),updatedAt: new Date()},
+     { username: 'catlover2000',hashedPassword: bcrypt.hashSync('P@ssw0rd', 10),firstName: "Blake",lastName: "Kindly",bio: "I just love CATS!!!!!!!!.",createdAt: new Date(),updatedAt: new Date()},
   ], {});
   },
 
